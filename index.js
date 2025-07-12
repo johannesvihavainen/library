c66aec76c3aa943c1789620d9412f7d6af3c11e3
Johannes Vihavainen <johannesvihavainen@icloud.com>
2025-07-12 16:03:12 +0300

    implement new book form. remove book button, and toggle read status feature

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const pages = document.querySelector('.pages');
 
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.id = crypto.randomUUID();
+    this.read = false;
+}
+
+Book.prototype.toggleStatus = function () {
+    return this.read = !this.read
 }
 
 function addBookToLibrary(title, author, pages) {
@@ -23,20 +28,52 @@ addBookToLibrary('Rich dad, Poor dad', 'Robert Kyosake', 400)
 addBookToLibrary('Atomic Habits', 'James Clear', 330)
 addBookToLibrary('Psycho-Cybernetics', 'Maxwell Maltz', 380)
 
-console.log(myLibrary);
-
 function updateBooks() {
     library.innerText = ''
     for (let i = 0; i < myLibrary.length; i++) {
         const book = document.createElement('div')
         book.classList.add('book')
+
+        book.dataset.id = myLibrary[i].id
+
         book.innerText = `
         ${myLibrary[i].title}
         ${myLibrary[i].author}
         ${myLibrary[i].pages} pages
+        Read: ${myLibrary[i].read ? "Yes" : "No"}
         `
         library.appendChild(book)
     }
+
+    const book = document.querySelectorAll('.book')
+
+    book.forEach(item => {
+        const removeBtn = document.createElement('button')
+        removeBtn.textContent = 'Remove'
+        removeBtn.classList.add('remove-btn')
+        item.appendChild(removeBtn)
+
+        const toggleStatusBtn = document.createElement('button')
+        toggleStatusBtn.textContent = 'toggle read'
+        toggleStatusBtn.classList.add('toggle-status-btn')
+        item.appendChild(toggleStatusBtn)
+
+        removeBtn.addEventListener('click', () => {
+            const bookId = item.dataset.id
+
+            myLibrary = myLibrary.filter(function (obj) {
+                return obj.id !== bookId
+            })
+            updateBooks()
+        })
+
+        toggleStatusBtn.addEventListener('click', () => {
+            const bookId = item.dataset.id
+            const bookToToggle = myLibrary.find(book => book.id === bookId)
+            bookToToggle.toggleStatus()
+            updateBooks()
+        })
+    });
 }
 
 function displayForm() {
@@ -71,3 +108,7 @@ addBookBtn.addEventListener('click', () => {
 })
 
 updateBooks()
+
+
+
+
